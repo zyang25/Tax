3666a126b88624619fe6d42e6cb23f7820503065
Charles Yang <zyang7253@gmail.com>
2015-10-16 15:54:21 -0400

    v1

--- README.md
+++
@@ -1,5 +0,0 @@
-# Tax
-Spring Rest service to build Tax Calculator
-
-EC2 public IP address: 54.193.93.188
-
---
+++ xml_parser.php
@@ -0,0 +1,71 @@
+<?php
+$myfile = fopen("newbeans.xml", "w") or die("Unable to open file!");
+
+function xml(){
+	$xml=simplexml_load_file("Beans.xml") or die("Error: Cannot open xml");	
+	//Bean node
+	$bean = $xml->addChild('bean');
+	$bean->addAttribute('id','personalInfo');
+	$bean->addAttribute('class','com.charles.app.personalInformation');
+	
+	//Property node
+	$property1 = $bean->addChild('property');
+	$property1->addAttribute('name','firstName');
+	$property1->addChild('value',$_POST['first_name']);
+	
+	//Property node
+	$property2 = $bean->addChild('property');
+	$property2->addAttribute('name','lastName');
+	$property2->addChild('value',$_POST['last_name']);
+
+	//Property node
+	$property3 = $bean->addChild('property');
+	$property3->addAttribute('name','idNumber');
+	$property3->addChild('value',$_POST['id']);
+
+	//Property node
+	$property4 = $bean->addChild('property');
+	$property4->addAttribute('name','presentHomeAdd');
+	$property4->addChild('value',$_POST['address1']);
+
+	//Property node
+	$property4 = $bean->addChild('property');
+	$property4->addAttribute('name','presentHomeAdd2');
+	$property4->addChild('value',$_POST['address2']);
+
+	//Property node
+	$property5 = $bean->addChild('property');
+	$property5->addAttribute('name','foreignCountryName');
+	$property5->addChild('value',$_POST['fcn']);
+
+	//Property node
+	$property6 = $bean->addChild('property');
+	$property6->addAttribute('name','foreignProvince');
+	$property6->addChild('value',$_POST['fp']);
+
+	//Property node
+	$property7 = $bean->addChild('property');
+	$property7->addAttribute('name','foreignPostCode');
+	$property7->addChild('value',$_POST['fpc']);
+
+	fwrite($GLOBALS['myfile'], $xml->saveXML());
+	
+	return $xml->saveXML();
+}
+function postXML(){
+			$xmldata = xml();
+			echo $xmldata;
+			$ch = curl_init('http://localhost:8181/test');
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/xml'));
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $xmldata);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			$output = curl_exec($ch);
+			curl_close($ch);
+}
+//if(isset($_POST['first_name'],$_POST['last_name'],$_POST['id'],$_POST['address'],$_POST['city'],$_POST['state'])){
+	postXML();
+//}
+?>
