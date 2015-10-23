dc7f9f3c29b0db37f531095084fea0f0f205b6f1
Charles Yang <zyang7253@gmail.com>
2015-10-23 10:27:21 -0400

    Final version of backend

--- src/main/java/com/charles/app/PersonController.java
+++ src/main/java/com/charles/app/PersonController.java
@@ -1,13 +1,19 @@
 package com.charles.app;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,7 +37,6 @@ public class PersonController {
     	Blanks blank = (Blanks)ac.getBean("tax");
     	PdfNameReader.nameReader();
         TextFields example = new TextFields(0);
-        //example.createPdf(RESULT1);
         example.manipulatePdf(TextFields.RESULT1, TextFields.RESULT2,pi,blank);
     }
     
@@ -61,6 +66,24 @@ public class PersonController {
     	      new HttpHeaders(), 
     	      HttpStatus.OK);
     }
+    
+    @RequestMapping(value = "/getPDF", method = RequestMethod.GET, produces = "application/pdf")
+    public ResponseEntity<InputStreamResource> downloadPDFFile()
+            throws IOException {
+    	try{
+    		FileSystemResource pdfFile = new FileSystemResource("1040result.pdf");
+    			return ResponseEntity
+                    .ok()
+                    .contentLength(pdfFile.contentLength())
+                    .contentType(
+                            MediaType.parseMediaType("application/octet-stream"))
+                    .body(new InputStreamResource(pdfFile.getInputStream()));
+    	}catch(Exception e){    		
+    		return null;
+    	}
+        
+    }
+    
     
     public static void stringToDom(String xmlSource) 
             throws IOException {
