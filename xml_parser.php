ac211a7e0258a4c727d9bd21f7cc278707c4ea6c
Charles Yang <zyang7253@gmail.com>
2015-10-23 09:00:39 -0400

    M

--- xml_parser.php
+++ xml_parser.php
@@ -173,8 +173,8 @@ postXML();
 <script type="text/javascript">
     window.onload = function (){
       var myPDF = new PDFObject({ url: "http://54.193.93.188:8181/getPDF" }).embed();
-      if(myPDF){
-  			//do something
+      if(!myPDF){
+  			
 		}	
     };
 </script>
